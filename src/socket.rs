9ee293c27f860a058a07028da219d96cf94ad33e
FAMASoon <famasoon@gmail.com>
2023-01-08 06:57:33 +0000

    Init some packet files

---
+++ src/socket.rs
@@ -0,0 +1,11 @@
+use crate::packet::TcpPacket;
+use crate::tcpflags;
+use anyhow::{Context, Result};
+use pnet::packet::{ip::IpNextHeaderProtocol, Packet};
+use pnet::transport::{self, TransportChannelType, TransportProtocol, TransportSender};
+use pnet::util;
+use std::collections::VecDeque;
+use std::fmt::{self, Display};
+use std::net::{IpAddr, Ipv4Addr};
+use std::time::SystemTime;
+const SOCKET_BUFFER_SIZE: usize = 4380;
